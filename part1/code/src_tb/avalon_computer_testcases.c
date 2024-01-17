5d83ad73af50d9a2bcd4b5c7719bc8891aac7b68
leandro <leandro262009@gmail.com>
2024-01-17 15:14:12 +0100

    Added C testcases

--- part1/code/src_tb/avalon_computer_testcases.c
+++ part1/code/src_tb/avalon_computer_testcases.c
@@ -1,24 +1,51 @@
-
-
 #include "stdio.h"
 #include "avalon_computer_testcases.h"
 
+#define OFFSET_A        0
+#define OFFSET_B        1
+#define OFFSET_C        2
+#define OFFSET_RESULT   3
+#define OFFSET_CMD      4
+#define OFFSET_STATUS   5
+
+#define BYTE_ENABLE     0x11  // Mask defining what bytes are active
+#define AV_WRITE(adr, data) if (avalon_write(adr, BYTE_ENABLE, data)) { printf("Error during avalon write\n"); return 1; }
+#define AV_READ(adr, data) if (avalon_write(adr, BYTE_ENABLE, data)) { printf("Error during avalon read\n"); return 1; }
+
+int compute(int n, int a, int b, int c) {
+    return a^n + b * c;
+}
+
+void testcase(int n, int a, int b, int c) {
+    int data
+
+    // Write val a
+    AV_WRITE(OFFSET_A, a);
+
+    // Write val b
+    AV_WRITE(OFFSET_B, b);
+
+    // Write val c
+    AV_WRITE(OFFSET_C, c);
+
+    // Start computing
+    AV_WRITE(OFFSET_CMD, 0);
+
+    // Start computing
+    AV_WRITE(OFFSET_CMD, 0);
+
+    // Wait for the status to be ok
+    while (!status) AV_READ(OFFSET_RESULT, &status);
+
+    // Read the results
+    AV_READ(OFFSET_RESULT, &data);
+
+    printf("\n");
+}
 
 DPI_DLLESPEC
 int CTask()
 {
-    for (int i = 0; i < 10; i++) {
-        if (avalon_write(0, 3, i)) {
-            return 1;
-        }
-
-        unsigned int data;
-        if (avalon_read(0, 3, &data)) {
-            return 1;
-        }
-        if (data != i) {
-            printf("Error. Expected %d, got %d\n", i, data);
-        }
-    }
+    
     return 0;
 }
